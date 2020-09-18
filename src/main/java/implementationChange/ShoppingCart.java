73e12946aebd26e4794d81c740df6513aa33540d
josnih <josnih@gmail.com>
2020-09-18 09:57:04 +0100

    Add prices list shopping cart :white_check_mark:

--- src/main/java/implementationChange/ShoppingCart.java
+++ src/main/java/implementationChange/ShoppingCart.java
@@ -1,14 +1,19 @@
 package implementationChange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
     private int price;
+    private final List<Integer> prices = new ArrayList<>();
 
     public void add(int price) {
         this.price = price;
+        prices.add(price);
     }
 
     public int calculateTotalPrice() {
-        return price;
+        return prices.stream().mapToInt(Integer::intValue).sum();
     }
 
     public boolean hasDiscount() {
