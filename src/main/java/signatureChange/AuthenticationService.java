53a6fd50ea000b5fca6208d9400b9993cd636c3d
josnih <josnih@gmail.com>
2020-09-18 11:22:28 +0100

    Revert "a solution for auth service that breaks tests :poop:"
    This reverts commit 0716ed1b275549aa1243b335c952fec502055c7c.

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -1,10 +1,7 @@
 package signatureChange;
 
 public class AuthenticationService {
-//    public boolean isAuthenticated(int id) {
-//        return id == 12345;
-//    }
-    public boolean isAuthenticated(Id id) {
-        return id.isAdmin();
+    public boolean isAuthenticated(int id) {
+        return id == 12345;
     }
 }
--- src/test/java/unit/AuthenticationServiceShould.java
+++ src/test/java/unit/AuthenticationServiceShould.java
@@ -3,18 +3,19 @@ package unit;
 import signatureChange.AuthenticationService;
 import org.junit.Assert;
 import org.junit.Test;
-import signatureChange.Id;
 
 public class AuthenticationServiceShould {
 
     @Test
     public void distinguish_administrator_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
-        Assert.assertTrue(service.isAuthenticated(new Id(12345)));
+        int adminId = 12345;
+        Assert.assertTrue(service.isAuthenticated(adminId));
     }
     @Test
     public void distinguish_non_admin_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
-        Assert.assertFalse(service.isAuthenticated(new Id(1111)));
+        int normalUserId = 11111;
+        Assert.assertFalse(service.isAuthenticated(normalUserId));
     }
 }
